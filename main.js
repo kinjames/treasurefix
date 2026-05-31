2f989bf9d8aaf001fb06983023dfc247a3798d90
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2026-06-01 00:41:19 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ function toggleMenu() {
   const nav = document.getElementById("navLinks");
   nav.classList.toggle("active");
 }
+
+const links = document.querySelectorAll("#navLinks a");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.getElementById("navLinks").classList.remove("active");
+  });
+});
